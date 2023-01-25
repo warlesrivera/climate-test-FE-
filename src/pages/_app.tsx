3ab9261b03cb5351b01b09922ffe331d7c7413fc
Warless Rivera <Warless@moxhub.com>
2023-01-25 11:22:44 -0500

    config redux toolkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Provider as ReduxProvider } from 'react-redux';
 import { lightTheme } from '../themes';
+import { store } from '../store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme }>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>)
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ReduxProvider>)
 }
---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import { rootReducer } from "./root-reducer";
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat()
+ })
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
