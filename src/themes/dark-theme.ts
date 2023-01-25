344fe8398194ac2c02b68244e5f70e8dbcca5465
Warless Rivera <Warless@moxhub.com>
2023-01-25 11:17:23 -0500

    add material UI and react toolkit

---
+++ src/themes/dark-theme.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme(
+  {
+    palette: {
+      mode: 'dark'
+    },
+    components: {
+      MuiAppBar: {
+        defaultProps: {
+            elevation:0
+        },
+        styleOverrides: {
+            root: {
+                backgroundColor: '#4a148c',
+                
+            }
+        }
+    }
+    }
+  }
+);  
